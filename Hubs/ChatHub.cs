5e147f826e632eeab3e15e7b7393aeba0acbeeb0
Seth Dana <seth.dana@gmail.com>
2018-10-14 15:03:45 -0500

    initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PleaseWork.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task ChatShit(string message)
+        {
+           await Clients.Others.SendAsync(message);
+        }
+
+        override public async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("works");
+        }
+
+        public async Task NewClick()
+        {
+            await Clients.All.SendAsync("youClicked");
+        }
+    }
+}
